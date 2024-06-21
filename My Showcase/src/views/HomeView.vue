90024540e363bf72afadfdfab955fc595896c44c
URATACO <95185179+URATACO@users.noreply.github.com>
2024-06-21 15:45:27 -0400

    Updates

--- My Showcase/src/views/HomeView.vue
+++ My Showcase/src/views/HomeView.vue
@@ -4,6 +4,7 @@
       Welcome to my page. And excuse the dust while things get setup here!
       Currently switching over to vue. NOTE: All projects are currently disabled and cannot be accesed during this transition.
     </h1>
+    <temp v-if="showSecret"/>
     <project-widg/>
     <div>
       
@@ -23,10 +24,29 @@
 
 <script>
   import projectwidg from "../components/ProjectsWidget.vue"
+  import temp from "../components/TempQR.vue"
   export default{
     components: {
-      'projectWidg': projectwidg
+      'projectWidg': projectwidg,
+      'temp': temp
+    },
+    data() {
+    return {
+      showSecret: false
+    };
+  },
+  mounted() {
+    // Expose the Mark Function???
+    window.mark = this.mark;
+  },
+  methods: {
+    mark() {
+      this.showSecret = !this.showSecret;
+      console.log(
+        `QR component is now ${this.showSecret ? 'visible' : 'hidden'} Now Get Those Leads!!!.`
+      );
     }
+  }
   }
 </script>
 
