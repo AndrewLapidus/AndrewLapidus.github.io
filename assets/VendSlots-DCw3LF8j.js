30eb2851494e9856690b034bcf5d03f6f097bb44
URATACO <95185179+URATACO@users.noreply.github.com>
2024-12-05 22:30:08 -0500

    Updates

---
+++ assets/VendSlots-DCw3LF8j.js
@@ -0,0 +1,1 @@
+import{_ as s,o,c as t,a as e}from"./index-DwHxrjbM.js";const c={},r={id:"projectsWidgetMain"},a=e("div",{class:"record_wrap"},[e("div",{class:"record"}," this is a record ")],-1),d=[a];function n(_,i,p,l,f,h){return o(),t("div",r,d)}const u=s(c,[["render",n]]);export{u as default};
