939021f88655bef1fb25efc628f565263a1a5eb5
URATACO <95185179+URATACO@users.noreply.github.com>
2025-03-20 22:31:02 -0400

    added tranquillesa or however you say it

---
+++ My Showcase/src/components/projects/tranquilla.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Page under construction</h1>
+    <p>
+        Nothing here but us chickens!
+    </p>
+    <img src="/tranquilla.png"/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
