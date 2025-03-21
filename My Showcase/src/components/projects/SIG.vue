6e387a8ebc77bfeccfb4bbb3cdec36142b309bc8
URATACO <95185179+URATACO@users.noreply.github.com>
2025-03-20 22:15:24 -0400

    Made extra changes and forgot to deploy site as well

---
+++ My Showcase/src/components/projects/SIG.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <iframe id="frame" src="https://www.saneisgrey.com/" title="Sane Is Grey"></iframe>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+iframe{
+    width: 100%;
+    height: 900px;
+    border: none;
+}
+</style>
