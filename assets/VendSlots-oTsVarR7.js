a1acbea5672f7c42dab35e5ea52c3fb5b3f0f3e8
URATACO <95185179+URATACO@users.noreply.github.com>
2025-03-20 22:15:39 -0400

    Updates

--- assets/VendSlots-oTsVarR7.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o,c as t,a as e}from"./index-m8HarG2C.js";const c={},r={id:"projectsWidgetMain"},a=e("div",{class:"record_wrap"},[e("div",{class:"record"}," this is a record ")],-1),d=[a];function n(_,i,p,l,f,h){return o(),t("div",r,d)}const u=s(c,[["render",n]]);export{u as default};
